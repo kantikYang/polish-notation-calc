ac6e623869552aada5575f8cb1017f36d09c98a5
kantikYang <mr.kant2001@mail.ru>
2023-12-02 17:50:16 +0300

    feat: react router init

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,18 @@ import App from './pages/App/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PolishNotationConvert from './pages/PolishNotation/PolishNotationConvert';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/polishNotation" element={<PolishNotationConvert />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
