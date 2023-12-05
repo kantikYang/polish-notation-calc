510af732d8e9078c40b175bcc5287f4ab259ff31
kantikYang <mr.kant2001@mail.ru>
2023-12-05 20:19:13 +0300

    fix: polish notation fixed

--- src/utils/polishNotation.ts
+++ src/utils/polishNotation.ts
@@ -1,5 +1,25 @@
 export let mainOperations = new Set(['÷', '×', '−', '+']);
 
+interface typeOfWeight {
+  [key: string]: number;
+}
+
+const opWeight: typeOfWeight = {
+  '÷': 3,
+  '×': 3,
+  '−': 2,
+  '+': 2,
+  '(': 1,
+};
+
+const checkWeight = (arr: string[], targ: string) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (opWeight[targ] >= opWeight[arr[i]]) {
+      return false;
+    }
+  }
+};
+
 export const polishNotation = (matshStr: string): string[] => {
   const stack = [];
   const res = [];
@@ -9,7 +29,18 @@ export const polishNotation = (matshStr: string): string[] => {
   for (let i = 0; i < curStr.length; i++) {
     if (Number(curStr[i]) === Number(curStr[i])) {
       res.push(curStr[i]);
-    } else if (mainOperations.has(curStr[i]) || curStr[i] === '(') {
+    } else if (mainOperations.has(curStr[i])) {
+      // if (stack.length === 0 || checkWeight(stack, curStr[i])) {
+      //   stack.push(curStr[i]);
+      // } else {
+      while (
+        stack.length > 0 &&
+        opWeight[stack[stack.length - 1]] >= opWeight[curStr[i]]
+      ) {
+        res.push(stack.pop()!);
+      }
+      stack.push(curStr[i]);
+    } else if (curStr[i] === '(') {
       stack.push(curStr[i]);
     } else if (curStr[i] === ')') {
       let j = stack.length - 1;
